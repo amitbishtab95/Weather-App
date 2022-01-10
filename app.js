661dc81baa7d2e8f66f267c7da42a585f59c1b63
Your Name <amitbishtab95@gmail.com>
2022-01-10 16:39:19 +0530

    api integration and css update

--- app.js
+++ app.js
@@ -12,9 +12,15 @@ window.addEventListener("load", () => {
             console.log(position);
             lon = position.coords.longitude;
             lat = position.coords.latitude;
-            // const proxy = "https://cors-anywhere.herokuapp.com/corsdemo"; generally used for proxy server 
-
-            const api = `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=${100}&appid=2ae4119c29e9679309e9100706dc8671`;
+            // const proxy = "https://cors-anywhere.herokuapp.com/corsdemo";
+            //  generally used for proxy server 
+            let api = "";
+            if (location.protocol == 'https:') {
+                api = `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=${100}&appid=2ae4119c29e9679309e9100706dc8671`;
+            }
+            else {
+                api = `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=${100}&appid=2ae4119c29e9679309e9100706dc8671`;
+            }
             fetch(api)
                 .then(response => {
                     console.log(response);
@@ -26,7 +32,14 @@ window.addEventListener("load", () => {
                     // console.log(name);
 
                     // this is to get api as per the current city name 
-                    const api2 = `http://api.openweathermap.org/data/2.5/weather?q=${name}&appid=2ae4119c29e9679309e9100706dc8671`;
+                    let api2 = "";
+                    if (location.protocol == 'http') {
+                        api2 = `http://api.openweathermap.org/data/2.5/weather?q=${name}&appid=2ae4119c29e9679309e9100706dc8671`;
+                    }
+                    else {
+                        api2 = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=2ae4119c29e9679309e9100706dc8671`;
+
+                    }
                     fetch(api2)
                         .then(r => {
                             console.log(r);
